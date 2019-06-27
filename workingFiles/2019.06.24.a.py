f5cc7175aee1f031ba3d32d92654eda90dc0d03e
lt <newton45@126.com>
2019-06-27 17:03:32 +0800

    led blink

--- workingFiles/2019.06.24.a.py
+++ workingFiles/2019.06.24.a.py
@@ -1,4 +1,13 @@
 sync_code_version = 2019.06.24.a
 
+
+from machine import Pin,PWM
+import time
 def run():
+	led = Pin(2, Pin.OUT)
 	print("Running!!!")
+	while 1:
+		led.value(1)
+		time.sleep(0.5)
+		led.value(0)
+		time.sleep(0.5)
